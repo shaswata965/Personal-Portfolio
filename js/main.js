bc18598bc228670a7124f8d16c54a57aa2a5d89b
shaswata965 <shaswata965@gmail.com>
2024-01-07 23:02:40 -0330

    Final Content Commit

--- js/main.js
+++ js/main.js
@@ -85,8 +85,8 @@
 	    margin:0,
 	    animateOut: 'fadeOut',
 	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
+	    nav:true,
+	    autoplayHoverPause: true,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
 	    responsive:{
@@ -109,7 +109,7 @@
 			items:1,
 			margin: 30,
 			stagePadding: 0,
-			nav: false,
+			nav: true,
 			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
 			responsive:{
 				0:{
@@ -120,6 +120,25 @@
 				},
 				1000:{
 					items: 3
+				}
+			}
+		});
+		$('.carousel-newsLetter').owlCarousel({
+			center: true,
+			loop: true,
+			autoplay: true,
+			autoplaySpeed:2000,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: true,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
 				}
 			}
 		});
